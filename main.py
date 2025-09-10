7280e455f560cee5b451b6dafb071c1bc5d53930
Igee Theron <igeetheron@gmail.com>
2025-09-10 17:37:24 +0200

    Removed sys from import since it is not used

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import argparse
 from dotenv import load_dotenv
 from google import genai
@@ -28,7 +27,7 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=args.prompt)
-    
+
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count }")
